6af4e54d37dbef33d96af0bb9664ee68df54c72d
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-03-10 23:11:13 -0500

    Added tslint-react.js rules

---
+++ src/tslint-react.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "jsx-alignment": true,
+  "jsx-ban-props": false,
+  "jsx-curly-spacing": [true, "always"],
+  "jsx-no-lambda": false,
+  "jsx-no-multiline-js": true,
+  "jsx-no-string-ref": true,
+  "jsx-self-close": true,
+  "jsx-use-translation-function": false,
+  "jsx-wrap-multiline": true
+};

ee28b92e606a4fb94c85f1bffe869852958c903b
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-04-06 10:02:20 -0400

    Removed microsoft-contrib
    There were breaking changes when upgrading to TSLint 5.0 and
    microsoft-contrib hasn't figured out how to handle that, yet, so I'm
    disabling it for now. Hopefully they get it fixed soon, because I was
    really enjoying their rules.

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const { dirname, join } = require('path');
 const { resolve } = require;
 
 const eslint = require('./src/tslint-eslint-rules');
-const microsoft_contrib = require('./src/tslint-microsoft-contrib');
+// const microsoft_contrib = require('./src/tslint-microsoft-contrib');
 const react = require('./src/tslint-react');
 const tslint = require('./src/tslint');
 
 module.exports = {
   "rulesDirectory": [
     join(dirname(resolve("tslint-eslint-rules")), '/dist/rules'),
-    dirname(resolve("tslint-microsoft-contrib")),
+//  dirname(resolve("tslint-microsoft-contrib")),
     join(dirname(resolve("tslint-react")), '/rules'),
     join(dirname(resolve("tslint")), '/rules')
   ],
-  "rules": Object.assign({}, eslint, microsoft_contrib, react, tslint)
+  "rules": Object.assign({}, eslint, /*microsoft_contrib,*/ react, tslint)
 }

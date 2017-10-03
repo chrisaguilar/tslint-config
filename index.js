00a56a64db323d71d699074a5f0fe8692c3337c8
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-10-03 13:58:31 -0400

    Removes tslint-eslint-rules.
    Just didn't really see the need.

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const { dirname, join } = require('path');
 const { resolve } = require;
 
-const eslint = require('./src/tslint-eslint-rules.json');
 const microsoft_contrib = require('./src/tslint-microsoft-contrib.json');
 const react = require('./src/tslint-react.json');
 const tslint = require('./src/tslint.json');
 
 module.exports = {
     "rulesDirectory": [
-        join(dirname(resolve("tslint-eslint-rules")), '/dist/rules'),
         dirname(resolve("tslint-microsoft-contrib")),
         join(dirname(resolve("tslint-react")), '/rules'),
         join(dirname(resolve("tslint")), '/rules')
     ],
-    "rules": Object.assign({}, eslint, microsoft_contrib, react, tslint)
+    "rules": Object.assign({}, microsoft_contrib, react, tslint)
 }
